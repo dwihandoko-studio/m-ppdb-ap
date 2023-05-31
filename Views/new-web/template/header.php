aa137b42a1f09d94b22cefc4febe5735b2ec400f
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-05-31 09:46:24 +0700

    change page home ppdb

--- Views/new-web/template/header.php
+++ Views/new-web/template/header.php
@@ -258,7 +258,7 @@
     <div class="close-btn"><i class="fas fa-times"></i></div>
 
     <nav class="menu-box">
-        <div class="nav-logo"><a href="index.html"><img src="<?= base_url('themes') ?>/images/logo.png" alt="" title=""></a></div>
+        <div class="nav-logo"><a href="index.html"><img src="<?= base_url('uploads/logo.png') ?>" alt="" title=""></a></div>
         <div class="menu-outer">
             <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
         </div>

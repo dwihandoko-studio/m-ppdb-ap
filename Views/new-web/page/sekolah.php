bebec6d070f84b9e1bc317a78040923be681777b
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-02 00:11:57 +0700

    change page home ppdb

--- Views/new-web/page/sekolah.php
+++ Views/new-web/page/sekolah.php
@@ -6,45 +6,41 @@
     <div class="container" style="margin-top: 100px;">
         <div class="inner-content">
             <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-12 service-block">
-                    <div class="service-block-three wow fadeInLeft animated" data-wow-delay="00ms" data-wow-duration="1500ms">
-                        <div class="inner-box">
-                            <div class="icon-box">
-                                <div class="bg-layer" style="background-image: url(<?= base_url('themes') ?>/images/icons/icon-bg-3.png);"></div>
-                                <i class="flaticon-profit"></i>
-                            </div>
-                            <h3><a href="#">Development Server</a></h3>
-                            <div class="text">Get Lightspeed Development Server for your website and fly in the web</div>
-                            <div class="link-btn"><a href="#"><i class="far fa-arrow-alt-circle-right"></i></a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 service-block">
-                    <div class="service-block-three wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
-                        <div class="inner-box">
-                            <div class="icon-box">
-                                <div class="bg-layer" style="background-image: url(<?= base_url('themes') ?>/images/icons/icon-bg-1.png);"></div>
-                                <i class="flaticon-shield-2"></i>
-                            </div>
-                            <h3><a href="#">Web Protection</a></h3>
-                            <div class="text">Best Protection and some tools are provided with our Web servers .</div>
-                            <div class="link-btn"><a href="#"><i class="far fa-arrow-alt-circle-right"></i></a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 service-block">
-                    <div class="service-block-three wow fadeInRight animated" data-wow-delay="00ms" data-wow-duration="1500ms">
-                        <div class="inner-box">
-                            <div class="icon-box">
-                                <div class="bg-layer" style="background-image: url(<?= base_url('themes') ?>/images/icons/icon-bg-4.png);"></div>
-                                <i class="flaticon-shopping-1"></i>
-                            </div>
-                            <h3><a href="#">E-commerce Shop</a></h3>
-                            <div class="text">You can build any kind of E-commerce Shop with payment security tools</div>
-                            <div class="link-btn"><a href="#"><i class="far fa-arrow-alt-circle-right"></i></a></div>
-                        </div>
-                    </div>
-                </div>
+                <?php if (isset($sekolahs)) { ?>
+                    <?php if (count($sekolahs) > 0) { ?>
+                        <?php foreach ($sekolahs as $key => $v) { ?>
+                            <?php if ($v->bentuk_pendidikan_id == 6) { ?>
+                                <div class="col-lg-4 col-md-6 col-sm-12 service-block">
+                                    <div class="service-block-three wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+                                        <div class="inner-box">
+                                            <div class="icon-box">
+                                                <div class="bg-layer" style="background-image: url(<?= base_url('themes') ?>/images/icons/icon-bg-1.png);"></div>
+                                                <i class="flaticon-school"></i>
+                                            </div>
+                                            <h3><a href="<?= base_url('web/sekolah/detail') . '?id=' . $v->id ?>"><?= $v->nama ?></a></h3>
+                                            <div class="text"><?= $v->npsn ?> - <?= $v->desa_kelurahan ?></div>
+                                            <div class="link-btn"><a href="<?= base_url('web/sekolah/detail') . '?id=' . $v->id ?>"><i class="far fa-arrow-alt-circle-right"></i></a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php } else { ?>
+                                <div class="col-lg-4 col-md-6 col-sm-12 service-block">
+                                    <div class="service-block-three wow fadeInLeft animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+                                        <div class="inner-box">
+                                            <div class="icon-box">
+                                                <div class="bg-layer" style="background-image: url(<?= base_url('themes') ?>/images/icons/icon-bg-3.png);"></div>
+                                                <i class="flaticon-school"></i>
+                                            </div>
+                                            <h3><a href="<?= base_url('web/sekolah/detail') . '?id=' . $v->id ?>"><?= $v->nama ?></a></h3>
+                                            <div class="text"><?= $v->npsn ?> - <?= $v->desa_kelurahan ?></div>
+                                            <div class="link-btn"><a href="<?= base_url('web/sekolah/detail') . '?id=' . $v->id ?>"><i class="far fa-arrow-alt-circle-right"></i></a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php } ?>
+                        <?php } ?>
+                    <?php } ?>
+                <?php } ?>
             </div>
         </div>
     </div>

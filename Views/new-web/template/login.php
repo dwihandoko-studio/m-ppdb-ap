95ee1df0cd1aa1fc1cc3aee2d576af52b0446c60
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-02 23:52:54 +0700

    change page home ppdb

--- Views/new-web/template/login.php
+++ Views/new-web/template/login.php
@@ -3,14 +3,14 @@
     <div class="popup-inner">
         <div class="container">
             <div class="donate-form-area" style="background: linear-gradient(130deg, #4527a4 15%, #ff0000 100%);">
-                <h2>Donation Information</h2>
-                <h4>How much would you like to donate:</h4>
+                <h2 style="color: #fff;">PPDB ONLINE TP. 2022 - 2023</h2>
+                <h4 style="color: #fff;"><span>Untuk Login Panitia Sekolah, silahkan login menggunakan email dan password yang sudah didaftarkan pada layanan.</span></h4>
                 <div id="content_block_51">
                     <div class="content-box">
                         <div class="donation-box wow fadeInUp animated" data-wow-delay="300ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 300ms; animation-name: fadeInUp;">
                             <div class="select-box">
                                 <div class="form-group">
-                                    <p style="color: #fff;">NISN / NIK</p>
+                                    <p style="color: #fff;">NISN / NIK / Email</p>
                                     <input style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 60px; outline: medium none;" type="text" name="_username" placeholder="">
                                 </div>
                                 <div class="form-group">

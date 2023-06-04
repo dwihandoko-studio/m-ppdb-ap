88853e1b57f9dc12ce8a74a93ad04f13bd9b1c59
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-04 22:30:04 +0700

    change page ppdb

--- Views/new-web/template/register.php
+++ Views/new-web/template/register.php
@@ -59,45 +59,51 @@
                 </div>
                 <div id="_belum_punya_nisn" class="_belum_punya_nisn" style="display: none;">
                     <h6 style="margin-bottom: 20px; color: #fff;background: rgb(255 255 255 / 19%);padding: 15px 20px; border-radius: 20px; box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);"><span>Untuk Peserta Yang Sudah Tercatat di Referensi Data Kemdikbud. (Berlaku untuk peserta PPDB yang akan mendaftar jenjang SD dan SMP). <a href="javascript:actionRegisterAfterSchool(this);" style="color: #00fff2; padding: 0px 10px;">Klik Disini...</a></span></h6>
-                    <form action="#" class="donate-form default-form">
-                        <div id="content_block_51">
-                            <div class="content-box">
-                                <div class="donation-box wow fadeInUp animated" data-wow-delay="300ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 300ms; animation-name: fadeInUp;">
-                                    <!-- <div class="select-box"> -->
-                                    <div class="row clearfix">
-                                        <div class="col-lg-6 col-md-6 col-sm-12">
-                                            <div class="form-group">
-                                                <p style="color: #fff;">Nama Lengkap</p>
-                                                <input type="text" name="_nama" id="_nama" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-6 col-md-6 col-sm-12">
-                                            <div class="form-group">
-                                                <p style="color: #fff;">NIK</p>
-                                                <input type="text" name="_nik" id="_nik" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-6 col-md-6 col-sm-12">
-                                            <div class="form-group">
-                                                <p style="color: #fff;">Password</p>
-                                                <input type="password" name="_password_regis" id="_password_regis" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-6 col-md-6 col-sm-12">
-                                            <div class="form-group">
-                                                <p style="color: #fff;">Ulangi Password</p>
-                                                <input type="password" name="_re_password_regis" id="_re_password_regis" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
-                                            </div>
+                    <!-- <form action="#" class="donate-form default-form"> -->
+                    <div id="content_block_51">
+                        <div class="content-box">
+                            <div class="donation-box wow fadeInUp animated" data-wow-delay="300ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 300ms; animation-name: fadeInUp;">
+                                <!-- <div class="select-box"> -->
+                                <div class="row clearfix">
+                                    <div class="col-lg-6 col-md-6 col-sm-12">
+                                        <div class="form-group">
+                                            <p style="color: #fff;">Nama Lengkap</p>
+                                            <input type="text" name="_nama" id="_nama" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
                                         </div>
                                     </div>
-                                    <!-- </div> -->
-                                    <div class="btn-box">
-                                        <button onclick="submitRegisterBeforeSchoolButton(this)" class="donate-box-btn _submit_login_button" id="_submit_login_button">DAFTAR</button>
+                                    <div class="col-lg-6 col-md-6 col-sm-12">
+                                        <div class="form-group">
+                                            <p style="color: #fff;">NIK</p>
+                                            <input type="text" name="_nik" id="_nik" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
+                                        </div>
                                     </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-12">
+                                        <div class="form-group">
+                                            <p style="color: #fff;">No Kartu Keluarga (KK)</p>
+                                            <input type="text" name="_kk" id="_kk" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-12">
+                                        <div class="form-group">
+                                            <p style="color: #fff;">Password</p>
+                                            <input type="password" name="_password_regis" id="_password_regis" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-12">
+                                        <div class="form-group">
+                                            <p style="color: #fff;">Ulangi Password</p>
+                                            <input type="password" name="_re_password_regis" id="_re_password_regis" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- </div> -->
+                                <div class="btn-box">
+                                    <button onclick="submitRegisterBeforeSchoolButton(this)" class="donate-box-btn _submit_login_button" id="_submit_login_button">DAFTAR</button>
                                 </div>
                             </div>
                         </div>
-                    </form>
+                    </div>
+                    <!-- </form> -->
                 </div>
             </div>
         </div>

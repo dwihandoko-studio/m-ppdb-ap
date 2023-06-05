7ed26a8f6a44b7e69302d2936c73355984a268d8
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-05 11:01:11 +0700

    fix detail home peserta

--- Controllers/Sekolah/Home.php
+++ Controllers/Sekolah/Home.php
@@ -7,11 +7,11 @@ use App\Libraries\Profilelib;
 use App\Libraries\Uuid;
 use Firebase\JWT\JWT;
 
-header("Access-Control-Allow-Origin: * ");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+// header("Access-Control-Allow-Origin: * ");
+// header("Content-Type: application/json; charset=UTF-8");
+// header("Access-Control-Allow-Methods: POST");
+// header("Access-Control-Max-Age: 3600");
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 class Home extends BaseController
 {

3245eecfa56a896cca82c4bb46e7790a95c23519
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-07 23:06:17 +0700

    add preview zonasi sekolah

--- Views/peserta/pendaftaran/zonasi/index-new.php
+++ Views/peserta/pendaftaran/zonasi/index-new.php
@@ -45,112 +45,112 @@
                         </form>
                     </div> -->
                     <div class="card-body">
-                        <div class="col-lg-12 content-pilihan-pendaftaran-zonasi-">
-                            <div class="row">
-                                <div class="col-lg-12 col-sm-12 col-md-12 mb-4">
-                                    <h4>Sekolah Pilihan 1 (Pertama) :</h4>
-                                    <input type="hidden" id="_sekolah_pilihan_pertama" name="_sekolah_pilihan_pertama" />
-                                    <?php if (isset($result)) { ?>
-                                        <?php if (count($result) > 0) { ?>
-                                            <?php foreach ($result as $key => $v) { ?>
-                                                <div class="col-lg-12" <?= ($key == 0) ? '' : 'style="margin-top: 60px;"' ?>>
-                                                    <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
-                                                            <div>
-                                                                <div class="row align-items-center">
-                                                                    <div class="col">
-                                                                        <h4 class="mb-0">
-                                                                            <a href="javascript:;">
-                                                                                <?= $v->nama ?>
-                                                                            </a>
-                                                                        </h4>
-                                                                        <small>NPSN: <?= $v->npsn ?></small>
-                                                                        <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $usernya->latitude, $usernya->longitude, 'kilometers') . ' Km' ?></small>
-                                                                        <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
-                                                                    </div>
+                        <!-- <div class="col-lg-12 content-pilihan-pendaftaran-zonasi-"> -->
+                        <div class="row">
+                            <div class="col-lg-12 col-sm-12 col-md-12 mb-4">
+                                <h4>Sekolah Pilihan 1 (Pertama) :</h4>
+                                <input type="hidden" id="_sekolah_pilihan_pertama" name="_sekolah_pilihan_pertama" />
+                                <?php if (isset($result)) { ?>
+                                    <?php if (count($result) > 0) { ?>
+                                        <?php foreach ($result as $key => $v) { ?>
+                                            <div class="col-lg-12" <?= ($key == 0) ? '' : 'style="margin-top: 60px;"' ?>>
+                                                <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
+                                                        <div>
+                                                            <div class="row align-items-center">
+                                                                <div class="col">
+                                                                    <h4 class="mb-0">
+                                                                        <a href="javascript:;">
+                                                                            <?= $v->nama ?>
+                                                                        </a>
+                                                                    </h4>
+                                                                    <small>NPSN: <?= $v->npsn ?></small>
+                                                                    <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $usernya->latitude, $usernya->longitude, 'kilometers') . ' Km' ?></small>
+                                                                    <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
                                                                 </div>
                                                             </div>
-                                                        </label>
-                                                    </div>
+                                                        </div>
+                                                    </label>
                                                 </div>
-                                            <?php } ?>
-                                        <?php } else { ?>
-                                            <p style="padding: 8px;">Tidak ada data sekolah dalam zonasi anda.</p>
+                                            </div>
                                         <?php } ?>
                                     <?php } else { ?>
                                         <p style="padding: 8px;">Tidak ada data sekolah dalam zonasi anda.</p>
                                     <?php } ?>
-                                </div>
-                                <hr />
-                                <div class="col-lg-12 col-sm-12 col-md-12 mb-4">
-                                    <h4>Sekolah Pilihan 2 (Kedua) :</h4>
-                                    <input type="hidden" id="_sekolah_pilihan_kedua" name="_sekolah_pilihan_kedua" />
-                                    <?php if (isset($result)) { ?>
-                                        <?php if (count($result) > 0) { ?>
-                                            <?php foreach ($result as $key => $v) { ?>
-                                                <div class="col-lg-12" <?= ($key == 0) ? '' : 'style="margin-top: 60px;"' ?>>
-                                                    <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
-                                                            <div>
-                                                                <div class="row align-items-center">
-                                                                    <div class="col ml--2">
-                                                                        <h4 class="mb-0">
-                                                                            <a href="javascript:;">
-                                                                                <?= $v->nama ?>
-                                                                            </a>
-                                                                        </h4>
-                                                                        <small>NPSN: <?= $v->npsn ?></small>
-                                                                        <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $usernya->latitude, $usernya->longitude, 'kilometers') . ' Km' ?></small>
-                                                                        <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
-                                                                    </div>
+                                <?php } else { ?>
+                                    <p style="padding: 8px;">Tidak ada data sekolah dalam zonasi anda.</p>
+                                <?php } ?>
+                            </div>
+                            <hr />
+                            <div class="col-lg-12 col-sm-12 col-md-12 mb-4">
+                                <h4>Sekolah Pilihan 2 (Kedua) :</h4>
+                                <input type="hidden" id="_sekolah_pilihan_kedua" name="_sekolah_pilihan_kedua" />
+                                <?php if (isset($result)) { ?>
+                                    <?php if (count($result) > 0) { ?>
+                                        <?php foreach ($result as $key => $v) { ?>
+                                            <div class="col-lg-12" <?= ($key == 0) ? '' : 'style="margin-top: 60px;"' ?>>
+                                                <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
+                                                        <div>
+                                                            <div class="row align-items-center">
+                                                                <div class="col ml--2">
+                                                                    <h4 class="mb-0">
+                                                                        <a href="javascript:;">
+                                                                            <?= $v->nama ?>
+                                                                        </a>
+                                                                    </h4>
+                                                                    <small>NPSN: <?= $v->npsn ?></small>
+                                                                    <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $usernya->latitude, $usernya->longitude, 'kilometers') . ' Km' ?></small>
+                                                                    <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
                                                                 </div>
                                                             </div>
-                                                        </label>
-                                                    </div>
+                                                        </div>
+                                                    </label>
                                                 </div>
-                                            <?php } ?>
-                                        <?php } else { ?>
-                                            <p style="padding: 8px;">Tidak ada data sekolah dalam zonasi anda.</p>
+                                            </div>
                                         <?php } ?>
                                     <?php } else { ?>
                                         <p style="padding: 8px;">Tidak ada data sekolah dalam zonasi anda.</p>
                                     <?php } ?>
-                                </div>
-                                <hr />
-                                <div class="col-lg-12 col-sm-12 col-md-12 mb-4">
-                                    <h4>Sekolah Pilihan 3 (Ketiga) :</h4>
-                                    <input type="hidden" id="_sekolah_pilihan_ketiga" name="_sekolah_pilihan_ketiga" />
-                                    <?php if (isset($result)) { ?>
-                                        <?php if (count($result) > 0) { ?>
-                                            <?php foreach ($result as $key => $v) { ?>
-                                                <div class="col-lg-12" <?= ($key == 0) ? '' : 'style="margin-top: 60px;"' ?>>
-                                                    <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
-                                                            <div>
-                                                                <div class="row align-items-center">
-                                                                    <div class="col ml--2">
-                                                                        <h4 class="mb-0">
-                                                                            <a href="javascript:;">
-                                                                                <?= $v->nama ?>
-                                                                            </a>
-                                                                        </h4>
-                                                                        <small>NPSN: <?= $v->npsn ?></small>
-                                                                        <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $usernya->latitude, $usernya->longitude, 'kilometers') . ' Km' ?></small>
-                                                                        <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
-                                                                    </div>
+                                <?php } else { ?>
+                                    <p style="padding: 8px;">Tidak ada data sekolah dalam zonasi anda.</p>
+                                <?php } ?>
+                            </div>
+                            <hr />
+                            <div class="col-lg-12 col-sm-12 col-md-12 mb-4">
+                                <h4>Sekolah Pilihan 3 (Ketiga) :</h4>
+                                <input type="hidden" id="_sekolah_pilihan_ketiga" name="_sekolah_pilihan_ketiga" />
+                                <?php if (isset($result)) { ?>
+                                    <?php if (count($result) > 0) { ?>
+                                        <?php foreach ($result as $key => $v) { ?>
+                                            <div class="col-lg-12" <?= ($key == 0) ? '' : 'style="margin-top: 60px;"' ?>>
+                                                <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
+                                                        <div>
+                                                            <div class="row align-items-center">
+                                                                <div class="col ml--2">
+                                                                    <h4 class="mb-0">
+                                                                        <a href="javascript:;">
+                                                                            <?= $v->nama ?>
+                                                                        </a>
+                                                                    </h4>
+                                                                    <small>NPSN: <?= $v->npsn ?></small>
+                                                                    <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $usernya->latitude, $usernya->longitude, 'kilometers') . ' Km' ?></small>
+                                                                    <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
                                                                 </div>
                                                             </div>
-                                                        </label>
-                                                    </div>
+                                                        </div>
+                                                    </label>
                                                 </div>
-                                            <?php } ?>
-                                        <?php } else { ?>
-                                            <p style="padding: 8px;">Tidak ada data sekolah dalam zonasi anda.</p>
+                                            </div>
                                         <?php } ?>
                                     <?php } else { ?>
                                         <p style="padding: 8px;">Tidak ada data sekolah dalam zonasi anda.</p>
                                     <?php } ?>
+                                <?php } else { ?>
+                                    <p style="padding: 8px;">Tidak ada data sekolah dalam zonasi anda.</p>
+                                <?php } ?>
 
-                                </div>
                             </div>
                         </div>
+                        <!-- </div> -->
                         <!-- <ul class="list-group list-group-flush list my--3 content_zonasi" id="content_zonasi">
 
                         </ul>

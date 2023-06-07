94f8fada1762a13796340eb84310eb0e7cf792f1
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-07 22:50:02 +0700

    add preview zonasi sekolah

--- Views/peserta/pendaftaran/zonasi/pilihan.php
+++ Views/peserta/pendaftaran/zonasi/pilihan.php
@@ -4,27 +4,29 @@
     <?php if (isset($result)) { ?>
         <?php if (count($result) > 0) { ?>
             <?php foreach ($result as $key => $v) { ?>
-                <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
-                        <div>
-                            <div class="row align-items-center">
-                                <div class="col-auto">
-                                    <a href="javascript:;" class="avatar" style="background-color: transparent;min-width: 100px;">
-                                        <img style="min-width: 100px;" alt="Image placeholder" src="<?= base_url('new-assets') ?>/assets/img/icons/<?= ((int)$v->status_sekolah == 1) ? 'sekolah-negeri.png' : 'sekolah-swasta.png' ?>">
-                                    </a>
-                                </div>
-                                <div class="col ml--2">
-                                    <h4 class="mb-0">
-                                        <a href="javascript:;">
-                                            <?= $v->nama ?>
+                <div>
+                    <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
+                            <div>
+                                <div class="row align-items-center">
+                                    <div class="col-auto">
+                                        <a href="javascript:;" class="avatar" style="background-color: transparent;min-width: 100px;">
+                                            <img style="min-width: 100px;" alt="Image placeholder" src="<?= base_url('new-assets') ?>/assets/img/icons/<?= ((int)$v->status_sekolah == 1) ? 'sekolah-negeri.png' : 'sekolah-swasta.png' ?>">
                                         </a>
-                                    </h4>
-                                    <small>NPSN: <?= $v->npsn ?></small>
-                                    <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $user->latitude, $user->longitude, 'kilometers') . ' Km' ?></small>
-                                    <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
+                                    </div>
+                                    <div class="col ml--2">
+                                        <h4 class="mb-0">
+                                            <a href="javascript:;">
+                                                <?= $v->nama ?>
+                                            </a>
+                                        </h4>
+                                        <small>NPSN: <?= $v->npsn ?></small>
+                                        <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $user->latitude, $user->longitude, 'kilometers') . ' Km' ?></small>
+                                        <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </label>
+                        </label>
+                    </div>
                 </div>
             <?php } ?>
         <?php } else { ?>
@@ -41,27 +43,29 @@
     <?php if (isset($result)) { ?>
         <?php if (count($result) > 0) { ?>
             <?php foreach ($result as $key => $v) { ?>
-                <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
-                        <div>
-                            <div class="row align-items-center">
-                                <div class="col-auto">
-                                    <a href="javascript:;" class="avatar" style="background-color: transparent;min-width: 100px;">
-                                        <img style="min-width: 100px;" alt="Image placeholder" src="<?= base_url('new-assets') ?>/assets/img/icons/<?= ((int)$v->status_sekolah == 1) ? 'sekolah-negeri.png' : 'sekolah-swasta.png' ?>">
-                                    </a>
-                                </div>
-                                <div class="col ml--2">
-                                    <h4 class="mb-0">
-                                        <a href="javascript:;">
-                                            <?= $v->nama ?>
+                <div>
+                    <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
+                            <div>
+                                <div class="row align-items-center">
+                                    <div class="col-auto">
+                                        <a href="javascript:;" class="avatar" style="background-color: transparent;min-width: 100px;">
+                                            <img style="min-width: 100px;" alt="Image placeholder" src="<?= base_url('new-assets') ?>/assets/img/icons/<?= ((int)$v->status_sekolah == 1) ? 'sekolah-negeri.png' : 'sekolah-swasta.png' ?>">
                                         </a>
-                                    </h4>
-                                    <small>NPSN: <?= $v->npsn ?></small>
-                                    <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $user->latitude, $user->longitude, 'kilometers') . ' Km' ?></small>
-                                    <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
+                                    </div>
+                                    <div class="col ml--2">
+                                        <h4 class="mb-0">
+                                            <a href="javascript:;">
+                                                <?= $v->nama ?>
+                                            </a>
+                                        </h4>
+                                        <small>NPSN: <?= $v->npsn ?></small>
+                                        <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $user->latitude, $user->longitude, 'kilometers') . ' Km' ?></small>
+                                        <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </label>
+                        </label>
+                    </div>
                 </div>
             <?php } ?>
         <?php } else { ?>
@@ -78,27 +82,29 @@
     <?php if (isset($result)) { ?>
         <?php if (count($result) > 0) { ?>
             <?php foreach ($result as $key => $v) { ?>
-                <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
-                        <div>
-                            <div class="row align-items-center">
-                                <div class="col-auto">
-                                    <a href="javascript:;" class="avatar" style="background-color: transparent;min-width: 100px;">
-                                        <img style="min-width: 100px;" alt="Image placeholder" src="<?= base_url('new-assets') ?>/assets/img/icons/<?= ((int)$v->status_sekolah == 1) ? 'sekolah-negeri.png' : 'sekolah-swasta.png' ?>">
-                                    </a>
-                                </div>
-                                <div class="col ml--2">
-                                    <h4 class="mb-0">
-                                        <a href="javascript:;">
-                                            <?= $v->nama ?>
+                <div>
+                    <div class="custom-control custom-radio mb-3"><input name="custom-radio-1" class="custom-control-input" id="customRadio5" type="radio"> <label class="custom-control-label" for="customRadio5">
+                            <div>
+                                <div class="row align-items-center">
+                                    <div class="col-auto">
+                                        <a href="javascript:;" class="avatar" style="background-color: transparent;min-width: 100px;">
+                                            <img style="min-width: 100px;" alt="Image placeholder" src="<?= base_url('new-assets') ?>/assets/img/icons/<?= ((int)$v->status_sekolah == 1) ? 'sekolah-negeri.png' : 'sekolah-swasta.png' ?>">
                                         </a>
-                                    </h4>
-                                    <small>NPSN: <?= $v->npsn ?></small>
-                                    <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $user->latitude, $user->longitude, 'kilometers') . ' Km' ?></small>
-                                    <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
+                                    </div>
+                                    <div class="col ml--2">
+                                        <h4 class="mb-0">
+                                            <a href="javascript:;">
+                                                <?= $v->nama ?>
+                                            </a>
+                                        </h4>
+                                        <small>NPSN: <?= $v->npsn ?></small>
+                                        <?= ((int)$v->status_sekolah == 1) ? '<span class="badge badge-success">Negeri</span>' : '<span class="badge badge-info">Swasta</span>' ?> &nbsp;&nbsp;<small>Jarak : <?= getJarak2Koordinat($v->latitude, $v->longitude, $user->latitude, $user->longitude, 'kilometers') . ' Km' ?></small>
+                                        <p style="margin: 0px; font-size: 11px;">Alamat: <?= $v->alamat_jalan ?>, Kec. <?= $v->nama_kecamatan ?> - <?= $v->nama_kabupaten ?> (<?= $v->nama_provinsi ?>)</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </label>
+                        </label>
+                    </div>
                 </div>
             <?php } ?>
         <?php } else { ?>

92a1e375ad2353057fc24b6edb88bc75b3e31bf6
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-11 23:21:59 +0700

    updated view zonasi sekolah

---
+++ Controllers/Web/Register.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controllers\Web;
+
+use App\Controllers\BaseController;
+use App\Models\Web\KuotaModel;
+use App\Models\Web\ZonasiModel;
+
+use App\Models\Web\RekapModel;
+use Config\Services;
+use App\Libraries\Profilelib;
+
+use App\Models\Dinas\Analisis\ProsesModel;
+use App\Models\Dinas\Analisis\ProsessekolahModel;
+use App\Models\Dinas\Analisis\ProsessekolahprosesModel;
+
+
+class Register extends BaseController
+{
+    var $folderImage = 'masterdata';
+    private $_db;
+    private $model;
+
+    function __construct()
+    {
+        helper(['text', 'file', 'form', 'cookie', 'session', 'array', 'imageurl', 'web', 'enskripdes', 'filesystem']);
+        $this->_db      = \Config\Database::connect();
+        // $this->session      = \Config\Database::connect();
+    }
+
+    public function index()
+    {
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        // var_dump($user);
+        // die;
+        if ($user->code == 200) {
+            $data['user'] = $user->data;
+        }
+
+        $data['page'] = "PPDB ONLINE TA. 2023 - 2024";
+        $data['title'] = 'PPDB ONLINE TA. 2023 - 2024';
+
+        return view('new-web/page/register', $data);
+    }
+}
--- Views/new-web/page/register.php
+++ Views/new-web/page/register.php
@@ -110,27 +110,93 @@
 <section class="donate-section elements">
     <div class="container">
         <div class="donate-form-area" style="background: none; border: none;">
-            <h2 style="color: #fff;">PPDB ONLINE TP. 2023 - 2024</h2>
-            <h6 style="margin-bottom: 20px; color: #fff;background: rgb(255 255 255 / 19%);padding: 15px 20px; border-radius: 20px; box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);"><span>Untuk Login Panitia Sekolah, silahkan login menggunakan email dan password yang sudah didaftarkan pada layanan.</span></h6>
-            <div id="content_block_51">
-                <div class="content-box">
-                    <div class="donation-box" style="visibility: visible;">
-                        <div class="select-box">
-                            <div class="form-group">
-                                <p style="color: #fff;">NISN / NIK</p>
-                                <input style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;" type="text" name="_username" id="_username" placeholder="">
+            <h2 style="color: #fff;">REGISTRASI - PPDB ONLINE TP. 2023 - 2024</h2>
+            <div id="_punya_nisn" class="_punya_nisn" style="display: block;">
+                <h6 style="margin-bottom: 20px; color: #fff;background: rgb(255 255 255 / 19%);padding: 15px 20px; border-radius: 20px; box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);"><span>Untuk Peserta Yang Belum Tercatat di Referensi Data Kemdikbud. (Berlaku hanya untuk peserta PPDB yang akan mendaftar jenjang SD). <a href="javascript:actionRegisterBeforeSchool(this);" style="color: #00fff2; padding: 0px 10px;">Klik Disini...</a></span></h6>
+                <!-- <form action="#" class="donate-form default-form"> -->
+                <div id="content_block_51">
+                    <div class="content-box">
+                        <div class="donation-box wow fadeInUp animated" data-wow-delay="300ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 300ms; animation-name: fadeInUp;">
+                            <!-- <div class="select-box"> -->
+                            <div class="row clearfix">
+                                <!-- <div class="col-lg-6 col-md-6 col-sm-12">
+                                        <div class="form-group">
+                                            <p style="color: #fff;">Jenjang PPDB</p>
+                                            <div class="select-box">
+                                                <select class="selectmenu1" name="_jenjang" id="_jenjang" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; width: 100%; font-weight: 400; height: 50px; outline: medium none; padding: 0px 10px;">
+                                                    <option selected="selected">SD</option>
+                                                    <option>SMP</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div> -->
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <p style="color: #fff;">NISN</p>
+                                        <input type="text" onfocus="inputFocus(this)" name="_nisn" id="_nisn" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
+                                        <div class="help-block _nisn"></div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <p style="color: #fff;">NPSN</p>
+                                        <input type="text" onfocus="inputFocus(this)" name="_npsn" id="_npsn" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
+                                        <div class="help-block _npsn"></div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <p style="color: #fff;">Tanggal Lahir</p>
+                                        <input class="datepicker" type="date" onfocus="inputFocus(this)" name="_tgl_lahir" id="_tgl_lahir" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
+                                        <div class="help-block _tgl_lahir"></div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <p style="color: #fff;">Password</p>
-                                <input style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;" type="password" name="_password" id="_password" placeholder="">
-                                <p style="color: #fff;">Lupa password? <a style="color: #00fff2" href="<?= base_url('auth/lupapassword') ?>">Reset</a></p>
+                            <!-- </div> -->
+                            <div class="btn-box btncekdata" style="display: block;">
+                                <button onclick="submitRegisterAfterSchoolButton(this)" class="donate-box-btn" id="btncekdata">CEK DATA</button>
                             </div>
-                        </div>
-                        <div class="btn-box">
-                            <button onclick="submitLoginButton(this)" class="donate-box-btn _submit_login_button" id="_submit_login_button">MASUK</button>
+                            <div class="content-siswa" style="display: none;">
+                            </div>
+
                         </div>
                     </div>
                 </div>
+                <!-- </form> -->
+            </div>
+            <div id="_belum_punya_nisn" class="_belum_punya_nisn" style="display: none;">
+                <h6 style="margin-bottom: 20px; color: #fff;background: rgb(255 255 255 / 19%);padding: 15px 20px; border-radius: 20px; box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);"><span>Untuk Peserta Yang Sudah Tercatat di Referensi Data Kemdikbud. (Berlaku untuk peserta PPDB yang akan mendaftar jenjang SD dan SMP). <a href="javascript:actionRegisterAfterSchool(this);" style="color: #00fff2; padding: 0px 10px;">Klik Disini...</a></span></h6>
+                <!-- <form action="#" class="donate-form default-form"> -->
+                <div id="content_block_51">
+                    <div class="content-box">
+                        <div class="donation-box wow fadeInUp animated" data-wow-delay="300ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 300ms; animation-name: fadeInUp;">
+                            <!-- <div class="select-box"> -->
+                            <div class="row clearfix">
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <p style="color: #fff;">NIK</p>
+                                        <input type="text" onfocus="inputFocus(this)" name="_nik" id="_nik" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
+                                        <div class="help-block _nik"></div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <p style="color: #fff;">No Kartu Keluarga (KK)</p>
+                                        <input type="text" onfocus="inputFocus(this)" name="_kk" id="_kk" placeholder="" style="background: transparent; border: 1px solid #fff !important; border-radius: 10px; color: #fff; font-size: 16px; font-weight: 400; height: 50px; outline: medium none;">
+                                        <div class="help-block _kk"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- </div> -->
+                            <div class="btn-box btncekdatabelumsekolah" style="display: block;">
+                                <button onclick="submitRegisterBeforeSchoolButton(this)" class="donate-box-btn" id="btncekdatabelumsekolah">CEK DATA</button>
+                            </div>
+                            <div class="content-siswa-belum" style="display: none;">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- </form> -->
             </div>
         </div>
     </div>

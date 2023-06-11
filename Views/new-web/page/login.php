9d6adce793967e5d5b7e013cbbabe19240f23e80
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-11 23:14:29 +0700

    updated view zonasi sekolah

--- Views/new-web/page/login.php
+++ Views/new-web/page/login.php
@@ -109,7 +109,7 @@
 </div>
 <section class="donate-section elements">
     <div class="container">
-        <div class="donate-form-area">
+        <div class="donate-form-area" style="background: none; border: none;">
             <h2 style="color: #fff;">PPDB ONLINE TP. 2023 - 2024</h2>
             <h6 style="margin-bottom: 20px; color: #fff;background: rgb(255 255 255 / 19%);padding: 15px 20px; border-radius: 20px; box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);"><span>Untuk Login Panitia Sekolah, silahkan login menggunakan email dan password yang sudah didaftarkan pada layanan.</span></h6>
             <div id="content_block_51">

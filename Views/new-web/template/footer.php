6b6e79cf9feb29d1ab4617f87657280ea18c3be2
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-12 11:06:53 +0700

    change page webp

--- Views/new-web/template/footer.php
+++ Views/new-web/template/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer style-five">
     <div class="anim-icons">
-        <div class="icon icon-1"><img src="<?= base_url('themes') ?>/images/icons/pattern-21.png" alt=""></div>
+        <div class="icon icon-1"><img src="<?= base_url('themes') ?>/images/icons/pattern-21.webp" alt=""></div>
     </div>
     <div class="image-layer" style="background-image: url(<?= base_url('themes') ?>/images/icons/footer-bg-5.webp);"></div>
     <div class="footer-top">
@@ -9,7 +9,7 @@
                 <div class="row">
                     <div class="col-lg-6 col-md-6 col-sm-12 footer-column">
                         <div class="about-widget footer-widget">
-                            <figure class="footer-logo"><a href="<?= base_url() ?>"><img src="<?= base_url('themes') ?>/images/footer-logo-2.png" alt=""></a></figure>
+                            <figure class="footer-logo"><a href="<?= base_url() ?>"><img src="<?= base_url('themes') ?>/images/footer-logo-2.webp" alt=""></a></figure>
                             <div class="text">Layanan Kegiatan Penerimaan Peserta Didik Baru Dalam Jaringan Tahun Ajaran 2023/2024 Kabupaten Pesawaran.</div>
                             <h2 class="phone"><i class="fas fa-phone-volume"></i><a href="tel:081398091467">CS SMP(+62) 813-9809-1467</a></h2>
                             <h2 class="phone"><i class="fas fa-phone-volume"></i><a href="tel:081398091467">CS SD(+62) 813-9809-1467</a></h2>

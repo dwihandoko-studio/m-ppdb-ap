b3882d2e4179000f3c99f1c10be2592207938e49
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-15 01:20:26 +0700

    fix page pendaftaran skeolah

--- Controllers/Sekolah/Pendaftaran/Mutasi.php
+++ Controllers/Sekolah/Pendaftaran/Mutasi.php
@@ -160,7 +160,7 @@ class Mutasi extends BaseController
                 ->select("b.*, k.lampiran_kk, k.lampiran_lulus, k.lampiran_pernyataan, k.lampiran_prestasi, k.lampiran_afirmasi, k.lampiran_mutasi, k.lampiran_lainnya, a.id as id_pendaftaran, c.nama as nama_sekolah_asal, c.npsn as npsn_sekolah_asal, j.nama as nama_sekolah_tujuan, j.npsn as npsn_sekolah_tujuan, j.latitude as latitude_sekolah_tujuan, j.longitude as longitude_sekolah_tujuan, a.kode_pendaftaran, a.via_jalur, d.nama as nama_provinsi, e.nama as nama_kabupaten, f.nama as nama_kecamatan, g.nama as nama_kelurahan, h.nama as nama_dusun, i.nama as nama_bentuk_pendidikan")
                 ->join('_users_profil_tb b', 'a.peserta_didik_id = b.peserta_didik_id', 'LEFT')
                 ->join('ref_sekolah c', 'a.from_sekolah_id = c.id', 'LEFT')
-                ->join('ref_sekolah j', 'a.tujuan_sekolah_id = j.id', 'LEFT')
+                ->join('ref_sekolah j', 'a.tujuan_sekolah_id_1 = j.id', 'LEFT')
                 ->join('ref_bentuk_pendidikan i', 'c.bentuk_pendidikan_id = i.id', 'LEFT')
                 ->join('ref_provinsi d', 'b.provinsi = d.id', 'LEFT')
                 ->join('ref_kabupaten e', 'b.kabupaten = e.id', 'LEFT')

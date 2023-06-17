eeb4950edcaf885372ae0b7a4ea067bbcf2448b1
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-17 21:24:15 +0700

    layout jadwal web

--- Views/new-web/page/jadwal.php
+++ Views/new-web/page/jadwal.php
@@ -74,7 +74,7 @@
                                                 Buka : <br>
                                                 <i class="far fa-calendar"></i> <?= tgl_indo($data->tgl_awal_verifikasi_zonasi) ?> <i class="far fa-clock"></i> Pukul <?= waktu_indo($data->tgl_awal_verifikasi_zonasi) ?> WIB<br>
                                                 Tutup : <br>
-                                                <i class="far fa-calendar"></i> <?= tgl_indo($data->tgl_awal_verifikasi_zonasi) ?> <i class="far fa-clock"></i> Pukul <?= waktu_indo($data->tgl_awal_verifikasi_zonasi) ?> WIB
+                                                <i class="far fa-calendar"></i> <?= tgl_indo($data->tgl_akhir_verifikasi_zonasi) ?> <i class="far fa-clock"></i> Pukul <?= waktu_indo($data->tgl_awal_verifikasi_zonasi) ?> WIB
                                             </li>
                                             <li>
                                                 <b>Analisis </b><br>

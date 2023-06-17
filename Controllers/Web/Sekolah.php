8073a1e988faaa5837bcd36a6200cd63ba77736d
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-18 01:18:06 +0700

    updated layout index

--- Controllers/Web/Sekolah.php
+++ Controllers/Web/Sekolah.php
@@ -296,6 +296,7 @@ class Sekolah extends BaseController
 
     public function index()
     {
+        return redirect()->to(base_url('web/home'));
     }
 
     public function kec()
@@ -314,7 +315,10 @@ class Sekolah extends BaseController
         $id_kec = htmlspecialchars($this->request->getGet('id'), true);
         $jenjang = htmlspecialchars($this->request->getGet('j'), true);
 
-        $data['sekolahs'] = $this->_db->table('ref_sekolah')->where(['kode_wilayah' => $id_kec, 'bentuk_pendidikan_id' => $jenjang])->orderBy('nama', 'asc')->get()->getResult();
+        $data['sekolahs'] = $this->_db->table('ref_sekolah a')
+            ->select("a.*")
+            ->join('_users_profil_tb b', 'a.id = b.sekolah_id')
+            ->where("LEFT(a.kode_wilayah,6) = '$id_kec' AND a.bentuk_pendidikan_id = '$jenjang'")->orderBy('a.nama', 'asc')->get()->getResult();
 
         // $data['jumlah_kuota'] = $this->_db->table('v_jumlah_kuota')
         //     ->select("SUM(zonasi) as zonasi, SUM(afirmasi) as afirmasi, SUM(prestasi) as prestasi, SUM(mutasi) as mutasi, SUM(pendaftar_zonasi_terverifikasi) as pendaftar_zonasi_terverifikasi, SUM(pendaftar_zonasi_antrian) as pendaftar_zonasi_antrian, SUM(pendaftar_afirmasi_terverifikasi) as pendaftar_afirmasi_terverifikasi, SUM(pendaftar_afirmasi_antrian) as pendaftar_afirmasi_antrian, SUM(pendaftar_mutasi_terverifikasi) as pendaftar_mutasi_terverifikasi, SUM(pendaftar_mutasi_antrian) as pendaftar_mutasi_antrian, SUM(pendaftar_prestasi_terverifikasi) as pendaftar_prestasi_terverifikasi, SUM(pendaftar_prestasi_antrian) as pendaftar_prestasi_antrian, SUM(pendaftar_swasta_terverifikasi) as pendaftar_swasta_terverifikasi, SUM(pendaftar_swasta_antrian) as pendaftar_swasta_antrian")

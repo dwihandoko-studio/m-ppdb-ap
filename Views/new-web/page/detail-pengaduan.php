2b0033c405f6d2847ac744d31ac295847cfc02f3
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-18 11:37:44 +0700

    change

--- Views/new-web/page/detail-pengaduan.php
+++ Views/new-web/page/detail-pengaduan.php
@@ -80,7 +80,7 @@
                                     <!-- <div class="replay-btn"><a href="#">Reply</a></div> -->
                                 </div>
                             </div>
-                            <hr />
+                            <hr style="margin-bottom: 20px;" />
                             <div class="comment replay-comment" style="margin-bottom: 30px;">
                                 <figure class="author-thumb"><img src="<?= base_url('themes') ?>/images/resource/comment-2.png" alt=""></figure>
                                 <div class="comment-inner">

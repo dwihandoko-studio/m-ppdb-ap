ab8453809d02229a901947177fcdf244a95c182c
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-20 03:34:03 +0700

    pembenahan

--- Views/new-web/page/detail-pencarian.php
+++ Views/new-web/page/detail-pencarian.php
@@ -23,10 +23,11 @@
                         <!-- <div class="text">
                             <p>Great code deserves an equally stunning visual representation, and this is what we deliver. Our Product Design team combines beautiful interfaces with captivating user experience. Top-notch blockchain developers, designers, and product owners - ready to build your product. </p>
                         </div> -->
-                        <?= $s = json_decode($siswa->details) ?>
+                        <?php //= $s = json_decode($siswa->details) 
+                        ?>
                         <ul class="info-list clearfix">
                             <li><span>NISN</span><?= $siswa->nisn ?></li>
-                            <li><span>NIK</span><?= $s->nik ?></li>
+                            <li><span>NIK</span><?= $s->details->nik ?></li>
                             <li><span>Live demo</span><a href="#">www.envato.com</a></li>
                         </ul>
                         <ul class="social-icons">

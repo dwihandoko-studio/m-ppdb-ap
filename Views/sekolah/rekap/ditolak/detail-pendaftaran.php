4c6633ae181eb5f43b424e66f9bd10bb10a361cf
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-20 21:54:07 +0700

    pembenahan

--- Views/sekolah/rekap/ditolak/detail-pendaftaran.php
+++ Views/sekolah/rekap/ditolak/detail-pendaftaran.php
@@ -153,6 +153,12 @@
                     </div>
                     <div class="col-md-3">
                         <div class="form-group _nama-block">
+                            <label for="_nama" class="form-control-label">Akta Kelahiran</label>
+                            <a target="_blank" href="<?= base_url('uploads/peserta/akta') . '/' . $data->lampiran_akta_kelahiran ?>" class="btn btn-block btn-info">Lampiran Akta Kelahiran</a>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group _nama-block">
                             <label for="_nama" class="form-control-label">Kartu Keluarga</label>
                             <a target="_blank" href="<?= base_url('uploads/peserta/kk') . '/' . $data->lampiran_kk ?>" class="btn btn-block btn-info">Lampiran Kartu Keluarga</a>
                         </div>

b4f52255faae7620e9080fafc6b391073b968acd
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-23 01:50:33 +0700

    pembenahan

--- Controllers/Dinas/Analisis/Prosesmutasi.php
+++ Controllers/Dinas/Analisis/Prosesmutasi.php
@@ -110,6 +110,7 @@ class Prosesmutasi extends BaseController
             $row[] = $list->nama_sekolah_tujuan;
             $row[] = $list->npsn_sekolah_tujuan;
             $row[] = $list->jumlah_pendaftar;
+            $row[] = $list->jumlah_kuota;
 
             $data[] = $row;
         }

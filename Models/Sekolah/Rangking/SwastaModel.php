6f1b5be20d3e89ca42a0f9049e5005b7fd7de979
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-24 15:32:51 +0700

    add peringkingan sekolah

--- Models/Sekolah/Rangking/SwastaModel.php
+++ Models/Sekolah/Rangking/SwastaModel.php
@@ -32,7 +32,7 @@ class SwastaModel extends Model
         $this->dt->select($select);
         $this->dt->join('_users_profil_tb b', 'a.peserta_didik_id = b.peserta_didik_id', 'LEFT');
         $this->dt->join('ref_sekolah c', 'a.from_sekolah_id = c.id', 'LEFT');
-        $this->dt->join('ref_sekolah j', 'a.tujuan_sekolah_id = j.id', 'LEFT');
+        $this->dt->join('ref_sekolah j', 'a.tujuan_sekolah_id_1 = j.id', 'LEFT');
         $this->dt->join('ref_bentuk_pendidikan i', 'c.bentuk_pendidikan_id = i.id', 'LEFT');
         // $this->dt->join('ref_provinsi d', 'b.provinsi = d.id', 'LEFT');
         // $this->dt->join('ref_kabupaten e', 'b.kabupaten = e.id', 'LEFT');
@@ -66,7 +66,7 @@ class SwastaModel extends Model
     function get_datatables($filter_jalur, $userId)
     {
         $this->_get_datatables_query();
-        $this->dt->where("a.tujuan_sekolah_id = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1)");
+        $this->dt->where("a.tujuan_sekolah_id_1 = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1)");
 
         if ($filter_jalur != "") {
             $this->dt->where('a.via_jalur', $filter_jalur);
@@ -80,7 +80,7 @@ class SwastaModel extends Model
     function count_filtered($filter_jalur, $userId)
     {
         $this->_get_datatables_query();
-        $this->dt->where("a.tujuan_sekolah_id = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1)");
+        $this->dt->where("a.tujuan_sekolah_id_1 = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1)");
 
         if ($filter_jalur != "") {
             $this->dt->where('a.via_jalur', $filter_jalur);
@@ -91,7 +91,7 @@ class SwastaModel extends Model
     public function count_all($filter_jalur, $userId)
     {
         $this->_get_datatables_query();
-        $this->dt->where("a.tujuan_sekolah_id = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1)");
+        $this->dt->where("a.tujuan_sekolah_id_1 = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1)");
 
         if ($filter_jalur != "") {
             $this->dt->where('a.via_jalur', $filter_jalur);

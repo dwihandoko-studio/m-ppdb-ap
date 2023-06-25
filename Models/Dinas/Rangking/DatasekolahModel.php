89bdbb812d64198954dc4bcb59ba392ffd3618b7
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-25 17:14:03 +0700

    add peringkingan sekolah

--- Models/Dinas/Rangking/DatasekolahModel.php
+++ Models/Dinas/Rangking/DatasekolahModel.php
@@ -26,12 +26,12 @@ class DatasekolahModel extends Model
     private function _get_datatables_query()
     {
 
-        $select = "a.id as id_pendaftaran, a.tujuan_sekolah_id, j.nama as nama_sekolah_tujuan, j.npsn as npsn_sekolah_tujuan, a.via_jalur, a.created_at, count(a.peserta_didik_id) as jumlah_pendaftar";  //14
+        $select = "a.id as id_pendaftaran, a.tujuan_sekolah_id_1, j.nama as nama_sekolah_tujuan, j.npsn as npsn_sekolah_tujuan, a.via_jalur, a.created_at, count(a.peserta_didik_id) as jumlah_pendaftar";  //14
 
         $this->dt->select($select);
         // $this->dt->join('_users_profil_tb b', 'a.peserta_didik_id = b.peserta_didik_id', 'LEFT');
         // $this->dt->join('ref_sekolah c', 'a.from_sekolah_id = c.id', 'LEFT');
-        $this->dt->join('ref_sekolah j', 'a.tujuan_sekolah_id = j.id', 'LEFT');
+        $this->dt->join('ref_sekolah j', 'a.tujuan_sekolah_id_1 = j.id', 'LEFT');
         // $this->dt->join('ref_bentuk_pendidikan i', 'c.bentuk_pendidikan_id = i.id', 'LEFT');
         // $this->dt->join('ref_provinsi d', 'b.provinsi = d.id', 'LEFT');
         // $this->dt->join('ref_kabupaten e', 'b.kabupaten = e.id', 'LEFT');
@@ -79,7 +79,7 @@ class DatasekolahModel extends Model
             $this->dt->where('j.bentuk_pendidikan_id', $filter_jenjang);
         }
 
-        $this->dt->groupBy('a.tujuan_sekolah_id');
+        $this->dt->groupBy('a.tujuan_sekolah_id_1');
 
         if ($this->request->getPost('length') != -1)
             $this->dt->limit($this->request->getPost('length'), $this->request->getPost('start'));
@@ -99,7 +99,7 @@ class DatasekolahModel extends Model
             $this->dt->where('j.bentuk_pendidikan_id', $filter_jenjang);
         }
 
-        $this->dt->groupBy('a.tujuan_sekolah_id');
+        $this->dt->groupBy('a.tujuan_sekolah_id_1');
 
         return $this->dt->countAllResults();
     }
@@ -116,7 +116,7 @@ class DatasekolahModel extends Model
             $this->dt->where('j.bentuk_pendidikan_id', $filter_jenjang);
         }
 
-        $this->dt->groupBy('a.tujuan_sekolah_id');
+        $this->dt->groupBy('a.tujuan_sekolah_id_1');
 
         return $this->dt->countAllResults();
     }

4e4c40b9ac3e214455d999f6e49854e25b9489ab
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-25 17:25:45 +0700

    pembenahan

---
+++ Controllers/Dinas/Rangking/Swasta.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace App\Controllers\Dinas\Rangking;
+
+use App\Controllers\BaseController;
+use App\Models\Dinas\Rangking\SwastaModel;
+use App\Models\Dinas\Rangking\DatasekolahModel;
+use Config\Services;
+
+use App\Libraries\Profilelib;
+use App\Libraries\Uuid;
+use App\Libraries\Sekolah\Riwayatlib;
+use Firebase\JWT\JWT;
+
+class Swasta extends BaseController
+{
+    var $folderImage = 'masterdata';
+    private $_db;
+    private $model;
+
+    function __construct()
+    {
+        helper(['text', 'file', 'form', 'session', 'array', 'imageurl', 'web', 'filesystem']);
+        $this->_db      = \Config\Database::connect();
+    }
+
+    public function getAll()
+    {
+        $request = Services::request();
+        $datamodel = new SwastaModel($request);
+
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->code != 200) {
+            delete_cookie('jwt');
+            session()->destroy();
+            return redirect()->to(base_url('web/home'));
+        }
+        if ($request->getMethod(true) == 'POST') {
+            $filter_jalur = htmlspecialchars($request->getVar('filter_jalur'), true) ?? "";
+            $sekolah_id = htmlspecialchars($request->getVar('sekolah_id'), true) ?? "";
+
+            $lists = $datamodel->get_datatables($filter_jalur, $sekolah_id);
+            // $lists = [];
+            $data = [];
+            $no = $request->getPost("start");
+            foreach ($lists as $list) {
+                $no++;
+                $row = [];
+
+                $action = '<button onclick="actionDetail(\'' . $list->id_pendaftaran . '\')" type="button" class="btn btn-primary btn-sm">
+                    <i class="fa fa-eye"></i>
+                    <span>Detail</span>
+                    </button>';
+                $row[] = $action;
+                $row[] = $no;
+
+                // $row[] = $list->jarak;
+                $row[] = $list->fullname;
+                $row[] = $list->nisn;
+                $row[] = $list->kode_pendaftaran;
+                $row[] = $list->via_jalur;
+                $row[] = $list->nama_sekolah_asal;
+                $row[] = (round($list->jarak, 2)) . ' Km';
+                // $row[] = getJarak2Koordinat($list->latitude, $list->longitude, $list->latitude_sekolah_tujuan, $list->longitude_sekolah_tujuan, 'kilometers') . ' Km';
+
+                $data[] = $row;
+            }
+            $output = [
+                "draw" => $request->getPost('draw'),
+                // "recordsTotal" => 0,
+                // "recordsFiltered" => 0,
+                "recordsTotal" => $datamodel->count_all($filter_jalur, $sekolah_id),
+                "recordsFiltered" => $datamodel->count_filtered($filter_jalur, $sekolah_id),
+                "data" => $data
+            ];
+            echo json_encode($output);
+        }
+    }
+
+    public function getAllSekolah()
+    {
+        $request = Services::request();
+        $datamodel = new DatasekolahModel($request);
+
+
+        $filterJenjang = htmlspecialchars($request->getVar('filter_jenjang'), true) ?? "";
+        $filterJalur = htmlspecialchars($request->getVar('filter_jalur'), true) ?? "";
+
+        $lists = $datamodel->get_datatables($filterJenjang, $filterJalur);
+        // $lists = [];
+        $data = [];
+        $no = $request->getPost("start");
+        foreach ($lists as $list) {
+            $no++;
+            $row = [];
+
+            $row[] = $no;
+            // if($hakAksesMenu) {
+            //     if((int)$hakAksesMenu->spj_tpg_verifikasi == 1) {
+            $action =
+                '
+                            <a target="_blank" href="' . base_url('dinas/rangking/swasta/sekolah') . '?token=' . $list->tujuan_sekolah_id_1 . '" class="btn btn-primary btn-sm">
+                                <i class="fa fa-eye"></i>
+                                <span>Detail</span>
+                            </a>';
+            $row[] = $action;
+            $row[] = $list->nama_sekolah_tujuan;
+            $row[] = $list->npsn_sekolah_tujuan;
+            $row[] = $list->jumlah_pendaftar;
+
+            $data[] = $row;
+        }
+        $output = [
+            "draw" => $request->getPost('draw'),
+            // "recordsTotal" => 0,
+            // "recordsFiltered" => 0,
+            "recordsTotal" => $datamodel->count_all($filterJenjang, $filterJalur),
+            "recordsFiltered" => $datamodel->count_filtered($filterJenjang, $filterJalur),
+            "data" => $data
+        ];
+        echo json_encode($output);
+    }
+
+    public function index()
+    {
+        return redirect()->to(base_url('dinas/rangking/swasta/data'));
+    }
+
+    public function data()
+    {
+        $data['title'] = 'Rekapitulasi Rangking Swasta';
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->code != 200) {
+            delete_cookie('jwt');
+            session()->destroy();
+            return redirect()->to(base_url('web/home'));
+        }
+
+        $data['user'] = $user->data;
+
+        // $data['provinsis'] = $this->_db->table('ref_provinsi')->whereNotIn('id', ['350000', '000000'])->orderBy('nama', 'asc')->get()->getResult();
+
+        return view('dinas/rangking/swasta/sekolah', $data);
+    }
+
+    public function sekolah()
+    {
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->code != 200) {
+            delete_cookie('jwt');
+            session()->destroy();
+            return redirect()->to(base_url('web/home'));
+        }
+
+        $data['user'] = $user->data;
+
+        $data['sekolah_id'] = htmlspecialchars($this->request->getGet('token'), true);
+        $data['sekolahname'] = $this->_db->table('ref_sekolah')->select("nama, npsn")->where('id', $data['sekolah_id'])->get()->getRowObject();
+        $data['title'] = $data['sekolahname']->nama . ' Rekapitulasi Rangking Afirmasi';
+
+        return view('dinas/rangking/swasta/index', $data);
+    }
+
+
+    public function detail()
+    {
+        if ($this->request->getMethod() != 'post') {
+            $response = new \stdClass;
+            $response->code = 400;
+            $response->message = "Permintaan tidak diizinkan";
+            return json_encode($response);
+        }
+
+        $rules = [
+            'id' => [
+                'rules' => 'required|trim',
+                'errors' => [
+                    'required' => 'Id tidak boleh kosong. ',
+                ]
+            ],
+        ];
+
+        if (!$this->validate($rules)) {
+            $response = new \stdClass;
+            $response->code = 400;
+            $response->message = $this->validator->getError('id');
+            return json_encode($response);
+        } else {
+            $id = htmlspecialchars($this->request->getVar('id'), true);
+
+            $oldData = $this->_db->table('_tb_pendaftar a')
+                ->select("b.*, k.lampiran_akta_kelahiran, k.lampiran_foto_rumah, k.lampiran_pernyataan, k.lampiran_kk, k.lampiran_lulus, k.lampiran_prestasi, k.lampiran_afirmasi, k.lampiran_mutasi, k.lampiran_lainnya, a.id as id_pendaftaran, c.nama as nama_sekolah_asal, c.npsn as npsn_sekolah_asal, j.nama as nama_sekolah_tujuan, j.npsn as npsn_sekolah_tujuan, j.latitude as latitude_sekolah_tujuan, j.longitude as longitude_sekolah_tujuan, a.kode_pendaftaran, a.via_jalur, d.nama as nama_provinsi, e.nama as nama_kabupaten, f.nama as nama_kecamatan, g.nama as nama_kelurahan, h.nama as nama_dusun, i.nama as nama_bentuk_pendidikan")
+                ->join('_users_profil_tb b', 'a.peserta_didik_id = b.peserta_didik_id', 'LEFT')
+                ->join('ref_sekolah c', 'a.from_sekolah_id = c.id', 'LEFT')
+                ->join('ref_sekolah j', 'a.tujuan_sekolah_id_1 = j.id', 'LEFT')
+                ->join('ref_bentuk_pendidikan i', 'c.bentuk_pendidikan_id = i.id', 'LEFT')
+                ->join('ref_provinsi d', 'b.provinsi = d.id', 'LEFT')
+                ->join('ref_kabupaten e', 'b.kabupaten = e.id', 'LEFT')
+                ->join('ref_kecamatan f', 'b.kecamatan = f.id', 'LEFT')
+                ->join('ref_kelurahan g', 'b.kelurahan = g.id', 'LEFT')
+                ->join('ref_dusun h', 'b.dusun = h.id', 'LEFT')
+                ->join('_upload_kelengkapan_berkas k', 'b.id = k.user_id', 'LEFT')
+                ->where('a.id', $id)
+                ->get()->getRowObject();
+
+            if (!$oldData) {
+                $response = new \stdClass;
+                $response->code = 400;
+                $response->message = "Data tidak ditemukan.";
+                return json_encode($response);
+            }
+
+            $data['data'] = $oldData;
+
+            $response = new \stdClass;
+            $response->code = 200;
+            $response->result = $oldData;
+            $response->data = view('dinas/rangking/swasta/detail', $data);
+            $response->message = "Data ditemukan.";
+            return json_encode($response);
+        }
+    }
+}
---
+++ Models/Dinas/Rangking/SwastaModel.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Models\Dinas\Rangking;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\Model;
+
+class SwastaModel extends Model
+{
+    protected $table = "_tb_pendaftar a";
+    protected $column_order = array('jarak', 'b.fullname', 'b.nisn', 'a.kode_pendaftaran', 'a.via_jalur', 'c.nama', 'jarak');
+    protected $column_search = array('b.fullname', 'b.nisn', 'c.nama', 'c.npsn', 'a.kode_pendaftaran');
+    // protected $order = array('jarak' => 'asc', 'c.nama' => 'asc');
+    protected $request;
+    protected $db;
+    protected $dt;
+
+    function __construct(RequestInterface $request)
+    {
+        parent::__construct();
+        $this->db = db_connect();
+        $this->request = $request;
+
+        $this->dt = $this->db->table($this->table);
+    }
+    private function _get_datatables_query()
+    {
+
+        // $select = "b.*, k.lampiran_kk, k.lampiran_lulus, k.lampiran_prestasi, k.lampiran_afirmasi, k.lampiran_mutasi, k.lampiran_lainnya, a.id as id_pendaftaran, c.nama as nama_sekolah_asal, c.npsn as npsn_sekolah_asal, j.nama as nama_sekolah_tujuan, j.npsn as npsn_sekolah_tujuan, j.latitude as latitude_sekolah_tujuan, j.longitude as longitude_sekolah_tujuan, a.kode_pendaftaran, a.via_jalur, d.nama as nama_provinsi, e.nama as nama_kabupaten, f.nama as nama_kecamatan, g.nama as nama_kelurahan, h.nama as nama_dusun, i.nama as nama_bentuk_pendidikan, ROUND(getDistanceKm(b.latitude,b.longitude,j.latitude,j.longitude), 2) AS jarak";  //14
+        $select = "b.*, a.id as id_pendaftaran, c.nama as nama_sekolah_asal, c.npsn as npsn_sekolah_asal, j.nama as nama_sekolah_tujuan, j.npsn as npsn_sekolah_tujuan, j.latitude as latitude_sekolah_tujuan, j.longitude as longitude_sekolah_tujuan, a.kode_pendaftaran, a.via_jalur, i.nama as nama_bentuk_pendidikan, ROUND(getDistanceKm(b.latitude,b.longitude,j.latitude,j.longitude), 2) AS jarak";  //14
+
+        $this->dt->select($select);
+        $this->dt->join('_users_profil_tb b', 'a.peserta_didik_id = b.peserta_didik_id', 'LEFT');
+        $this->dt->join('ref_sekolah c', 'a.from_sekolah_id = c.id', 'LEFT');
+        $this->dt->join('ref_sekolah j', 'a.tujuan_sekolah_id_1 = j.id', 'LEFT');
+        $this->dt->join('ref_bentuk_pendidikan i', 'c.bentuk_pendidikan_id = i.id', 'LEFT');
+        // $this->dt->join('ref_provinsi d', 'b.provinsi = d.id', 'LEFT');
+        // $this->dt->join('ref_kabupaten e', 'b.kabupaten = e.id', 'LEFT');
+        // $this->dt->join('ref_kecamatan f', 'b.kecamatan = f.id', 'LEFT');
+        // $this->dt->join('ref_kelurahan g', 'b.kelurahan = g.id', 'LEFT');
+        // $this->dt->join('ref_dusun h', 'b.dusun = h.id', 'LEFT');
+        // $this->dt->join('_upload_kelengkapan_berkas k', 'b.id = k.user_id', 'LEFT');
+
+        $i = 0;
+        foreach ($this->column_search as $item) {
+            if ($this->request->getPost('search')['value']) {
+                if ($i === 0) {
+                    $this->dt->groupStart();
+                    $this->dt->like($item, $this->request->getPost('search')['value']);
+                } else {
+                    $this->dt->orLike($item, $this->request->getPost('search')['value']);
+                }
+                if (count($this->column_search) - 1 == $i)
+                    $this->dt->groupEnd();
+            }
+            $i++;
+        }
+
+        $this->dt->orderBy('jarak', 'ASC');
+        $this->dt->orderBy('a.created_at', 'ASC');
+
+        if ($this->request->getPost('order')) {
+            $this->dt->orderBy($this->column_order[$this->request->getPost('order')['0']['column']], $this->request->getPost('order')['0']['dir']);
+        } else if (isset($this->order)) {
+            $order = $this->order;
+            $this->dt->orderBy(key($order), $order[key($order)]);
+        }
+    }
+    function get_datatables($filter_jalur, $userId)
+    {
+        $this->_get_datatables_query();
+        $this->dt->where("a.tujuan_sekolah_id_1 = '$userId' AND (a.status_pendaftaran = 1)");
+
+        if ($filter_jalur != "") {
+            $this->dt->where('a.via_jalur', $filter_jalur);
+        }
+
+        if ($this->request->getPost('length') != -1)
+            $this->dt->limit($this->request->getPost('length'), $this->request->getPost('start'));
+        $query = $this->dt->get();
+        return $query->getResult();
+    }
+    function count_filtered($filter_jalur, $userId)
+    {
+        $this->_get_datatables_query();
+        $this->dt->where("a.tujuan_sekolah_id_1 = '$userId' AND (a.status_pendaftaran = 1)");
+
+        if ($filter_jalur != "") {
+            $this->dt->where('a.via_jalur', $filter_jalur);
+        }
+
+        return $this->dt->countAllResults();
+    }
+    public function count_all($filter_jalur, $userId)
+    {
+        $this->_get_datatables_query();
+        $this->dt->where("a.tujuan_sekolah_id_1 = '$userId' AND (a.status_pendaftaran = 1)");
+
+        if ($filter_jalur != "") {
+            $this->dt->where('a.via_jalur', $filter_jalur);
+        }
+
+        return $this->dt->countAllResults();
+    }
+}

5ed5f622b8db7e951ec743998e1e2ffd58b7129e
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-06-27 17:16:06 +0700

    proses analisis

--- Controllers/Dinas/Analisis/Kuotasisa.php
+++ Controllers/Dinas/Analisis/Kuotasisa.php
@@ -42,6 +42,10 @@ class Kuotasisa extends BaseController
             foreach ($lists as $list) {
                 $no++;
                 $row = [];
+                $action = '
+                            <a target="_blank" href="' . base_url('dinas/analisis/kuotasisa/detail') . '?token=' . $list->tujuan_sekolah_id . '" class="btn btn-primary btn-sm">
+                                <i class="fa fa-eye"></i>
+                            </a>';
 
                 $row[] = $no;
                 $row[] = '<p>' . $list->nama_sekolah . '<br/>' . $list->nama_kecamatan . '</p>';
@@ -67,6 +71,43 @@ class Kuotasisa extends BaseController
         }
     }
 
+    public function getAll()
+    {
+        $request = Services::request();
+        $datamodel = new ProsesModel($request);
+
+
+        $filterJenjang = htmlspecialchars($request->getVar('filter_jenis'), true) ?? "";
+        $filterJalur = htmlspecialchars($request->getVar('filter_jalur'), true) ?? "";
+        $sekolah_id = htmlspecialchars($request->getVar('sekolah_id'), true) ?? "";
+
+        $lists = $datamodel->get_datatables($filterJenjang, $filterJalur, $sekolah_id);
+        // $lists = [];
+        $data = [];
+        $no = $request->getPost("start");
+        foreach ($lists as $list) {
+            $no++;
+            $row = [];
+            $row[] = $no;
+            $row[] = $list->fullname;
+            $row[] = $list->nisn;
+            $row[] = $list->via_jalur;
+            $row[] = $list->jarak . ' Km';
+            $row[] = $list->nama_sekolah_asal . ' (' . $list->npsn_sekolah_asal . ')';
+
+            $data[] = $row;
+        }
+        $output = [
+            "draw" => $request->getPost('draw'),
+            // "recordsTotal" => 0,
+            // "recordsFiltered" => 0,
+            "recordsTotal" => $datamodel->count_all($filterJenjang, $filterJalur, $sekolah_id),
+            "recordsFiltered" => $datamodel->count_filtered($filterJenjang, $filterJalur, $sekolah_id),
+            "data" => $data
+        ];
+        echo json_encode($output);
+    }
+
     public function index()
     {
         $data['title'] = 'Rekapitulasi Data Pelaksanaan';
@@ -81,5 +122,25 @@ class Kuotasisa extends BaseController
         $data['user'] = $user->data;
 
         return view('dinas/analisis/kuotasisa/index', $data);
+    }
+
+
+    public function detail()
+    {
+        $data['title'] = 'Rekapitulasi Proses Analysis';
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->code != 200) {
+            delete_cookie('jwt');
+            session()->destroy();
+            return redirect()->to(base_url('web/home'));
+        }
+
+        $data['user'] = $user->data;
+
+        $data['sekolah_id'] = htmlspecialchars($this->request->getGet('token'), true);
+        $data['sekolahname'] = $this->_db->table('ref_sekolah')->select("nama, npsn")->where('id', $data['sekolah_id'])->get()->getRowObject();
+
+        return view('dinas/analisis/kuotasisa/detail', $data);
     }
 }

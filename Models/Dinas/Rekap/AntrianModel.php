4b84802a4bbdd136bc9e8e83b835f37be98ca998
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-07-01 20:31:30 +0700

    updated proses riwayat daftar

--- Models/Dinas/Rekap/AntrianModel.php
+++ Models/Dinas/Rekap/AntrianModel.php
@@ -26,19 +26,20 @@ class AntrianModel extends Model
     private function _get_datatables_query()
     {
 
-        $select = "b.*, k.lampiran_kk, k.lampiran_lulus, k.lampiran_prestasi, k.lampiran_afirmasi, k.lampiran_mutasi, k.lampiran_lainnya, a.id as id_pendaftaran, c.nama as nama_sekolah_asal, c.npsn as npsn_sekolah_asal, j.nama as nama_sekolah_tujuan, j.npsn as npsn_sekolah_tujuan, j.latitude as latitude_sekolah_tujuan, j.longitude as longitude_sekolah_tujuan, a.kode_pendaftaran, a.via_jalur, d.nama as nama_provinsi, e.nama as nama_kabupaten, f.nama as nama_kecamatan, g.nama as nama_kelurahan, h.nama as nama_dusun, i.nama as nama_bentuk_pendidikan";
+        // $select = "b.*, k.lampiran_kk, k.lampiran_lulus, k.lampiran_prestasi, k.lampiran_afirmasi, k.lampiran_mutasi, k.lampiran_lainnya, a.id as id_pendaftaran, c.nama as nama_sekolah_asal, c.npsn as npsn_sekolah_asal, j.nama as nama_sekolah_tujuan, j.npsn as npsn_sekolah_tujuan, j.latitude as latitude_sekolah_tujuan, j.longitude as longitude_sekolah_tujuan, a.kode_pendaftaran, a.via_jalur, d.nama as nama_provinsi, e.nama as nama_kabupaten, f.nama as nama_kecamatan, g.nama as nama_kelurahan, h.nama as nama_dusun, i.nama as nama_bentuk_pendidikan";
+        $select = "b.fullname, b.nisn, i.nama as nama_bentuk_pendidikan, j.nama as nama_sekolah_tujuan, j.npsn as npsn_sekolah_tujuan, j.bentuk_pendidikan_id, a.id as id_pendaftaran, a.kode_pendaftaran, a.via_jalur";
 
         $this->dt->select($select);
         $this->dt->join('_users_profil_tb b', 'a.peserta_didik_id = b.peserta_didik_id', 'LEFT');
-        $this->dt->join('ref_sekolah c', 'a.from_sekolah_id = c.id', 'LEFT');
+        // $this->dt->join('ref_sekolah c', 'a.from_sekolah_id = c.id', 'LEFT');
         $this->dt->join('ref_sekolah j', 'a.tujuan_sekolah_id_1 = j.id', 'LEFT');
-        $this->dt->join('ref_bentuk_pendidikan i', 'c.bentuk_pendidikan_id = i.id', 'LEFT');
-        $this->dt->join('ref_provinsi d', 'b.provinsi = d.id', 'LEFT');
-        $this->dt->join('ref_kabupaten e', 'b.kabupaten = e.id', 'LEFT');
-        $this->dt->join('ref_kecamatan f', 'b.kecamatan = f.id', 'LEFT');
-        $this->dt->join('ref_kelurahan g', 'b.kelurahan = g.id', 'LEFT');
-        $this->dt->join('ref_dusun h', 'b.dusun = h.id', 'LEFT');
-        $this->dt->join('_upload_kelengkapan_berkas k', 'b.id = k.user_id', 'LEFT');
+        // $this->dt->join('ref_bentuk_pendidikan i', 'c.bentuk_pendidikan_id = i.id', 'LEFT');
+        // $this->dt->join('ref_provinsi d', 'b.provinsi = d.id', 'LEFT');
+        // $this->dt->join('ref_kabupaten e', 'b.kabupaten = e.id', 'LEFT');
+        // $this->dt->join('ref_kecamatan f', 'b.kecamatan = f.id', 'LEFT');
+        // $this->dt->join('ref_kelurahan g', 'b.kelurahan = g.id', 'LEFT');
+        // $this->dt->join('ref_dusun h', 'b.dusun = h.id', 'LEFT');
+        // $this->dt->join('_upload_kelengkapan_berkas k', 'b.id = k.user_id', 'LEFT');
 
         $i = 0;
         foreach ($this->column_search as $item) {

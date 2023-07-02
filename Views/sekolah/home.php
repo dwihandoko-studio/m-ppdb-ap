6d89c8b4969f2f3cf3365efd63bb332c0806d9a9
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-07-02 22:29:53 +0700

    updated proses riwayat daftar

--- Views/sekolah/home.php
+++ Views/sekolah/home.php
@@ -133,13 +133,13 @@
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="myModalPengumumanLabel">INFORMASI</h5>
-                                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
                                 <?= $pengumumans[0]->isi ?>
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary waves-effect" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-secondary waves-effect" data-dismiss="modal">Close</button>
                             </div>
                         </div>
                     </div>

c0841ed242402f6b3414b7f4d67ea80b01dfb424
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-07-02 21:54:24 +0700

    updated proses riwayat daftar

--- Views/new-web/page/index.php
+++ Views/new-web/page/index.php
@@ -290,7 +290,7 @@
     <?php if ($pengumuman) { ?>
         <div id="donate-popup" style="width: 70%; height: 70%;" class="donate-popup popup-visible">
             <div class="close-donate"><i class="far fa-window-close"></i></div>
-            <div class="popup-inner">
+            <div class="popup-inner" style="margin: 15% auto;">
                 <div class="container">
                     <div class="donate-form-area">
                         <h2>INFORMASI...!!!</h2>

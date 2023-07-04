d7bb1aaf82927c8d88c129018632dbcdb8803cf8
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-07-04 22:17:11 +0700

    updated proses riwayat daftar

--- Views/dinas/rangking/zonasi/index.php
+++ Views/dinas/rangking/zonasi/index.php
@@ -94,6 +94,21 @@
                 </div>
             </div>
         </div>
+        <div class="modal fade" id="contentModalUpdate" tabindex="-1" role="dialog" aria-labelledby="contentModalUpdateLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content modal-content-update-loading">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="contentModalUpdateLabel">Modal title</h5>
+                        <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button> -->
+                    </div>
+                    <div class="contentBodyModalUpdate">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <?= $this->endSection(); ?>

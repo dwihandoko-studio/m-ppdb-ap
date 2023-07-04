e025d57f4729f29d7f9b72bb4f13e52730287460
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-07-04 22:15:13 +0700

    updated proses riwayat daftar

--- Views/dinas/rekap/diverifikasi/index.php
+++ Views/dinas/rekap/diverifikasi/index.php
@@ -247,8 +247,8 @@
                 "orderable": false,
             }],
             lengthMenu: [
-                [10, 25, 50],
-                ['10 rows', '25 rows', '50 rows']
+                [10, 25, 50, -1],
+                ['10 rows', '25 rows', '50 rows', 'Show All']
             ],
             dom: 'Blfrtip',
             buttons: [

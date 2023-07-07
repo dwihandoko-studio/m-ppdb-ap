835089ad2731f2dde2013a16010cb4e26b46fbbf
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-07-07 09:58:12 +0700

    updated peringkat all jalur

--- Models/Sekolah/Rekap/DiverifikasiModel.php
+++ Models/Sekolah/Rekap/DiverifikasiModel.php
@@ -30,8 +30,8 @@ class DiverifikasiModel extends Model
 
         $this->dt->select($select);
         $this->dt->join('_users_profil_tb b', 'a.peserta_didik_id = b.peserta_didik_id', 'LEFT');
-        $this->dt->join('ref_sekolah c', 'a.from_sekolah_id = c.id', 'LEFT');
-        $this->dt->join('ref_sekolah j', 'a.tujuan_sekolah_id_1 = j.id', 'LEFT');
+        $this->dt->join('ref_sekolah_asal c', 'a.from_sekolah_id = c.id', 'LEFT');
+        $this->dt->join('ref_sekolah_tujuan j', 'a.tujuan_sekolah_id_1 = j.id', 'LEFT');
         $this->dt->join('ref_bentuk_pendidikan i', 'c.bentuk_pendidikan_id = i.id', 'LEFT');
         $this->dt->join('ref_provinsi d', 'b.provinsi = d.id', 'LEFT');
         $this->dt->join('ref_kabupaten e', 'b.kabupaten = e.id', 'LEFT');
@@ -65,7 +65,7 @@ class DiverifikasiModel extends Model
     function get_datatables($filterKecamatan, $filterKelurahan, $userId)
     {
         $this->_get_datatables_query();
-        $this->dt->where("a.tujuan_sekolah_id_1 = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1)");
+        $this->dt->where("a.tujuan_sekolah_id_1 = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1 OR a.status_pendaftaran = 2 OR a.status_pendaftaran = 3)");
 
         if ($filterKecamatan != "") {
             $this->dt->where('a.kecamatan', $filterKecamatan);
@@ -83,7 +83,7 @@ class DiverifikasiModel extends Model
     function count_filtered($filterKecamatan, $filterKelurahan, $userId)
     {
         $this->_get_datatables_query();
-        $this->dt->where("a.tujuan_sekolah_id_1 = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1)");
+        $this->dt->where("a.tujuan_sekolah_id_1 = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1 OR a.status_pendaftaran = 2 OR a.status_pendaftaran = 3)");
 
         if ($filterKecamatan != "") {
             $this->dt->where('a.kecamatan', $filterKecamatan);
@@ -98,7 +98,7 @@ class DiverifikasiModel extends Model
     public function count_all($filterKecamatan, $filterKelurahan, $userId)
     {
         $this->_get_datatables_query();
-        $this->dt->where("a.tujuan_sekolah_id_1 = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1)");
+        $this->dt->where("a.tujuan_sekolah_id_1 = (SELECT sekolah_id FROM _users_profil_tb WHERE id = '$userId') AND (a.status_pendaftaran = 1 OR a.status_pendaftaran = 2 OR a.status_pendaftaran = 3)");
 
         if ($filterKecamatan != "") {
             $this->dt->where('a.kecamatan', $filterKecamatan);
